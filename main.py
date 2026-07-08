8cd1b3d86f6f9442ea884900e7b1ba766e5cf7c2
yogi_kota <guangtaiyuyi@gmail.com>
2026-07-09 07:59:26 +0900

    CRUDを実装することができたが、編集できない問題が発生し、時間がかかったが解決できた

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-TXT_FILE = "tasks.txt"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TXT_FILE = os.path.join(BASE_DIR, "tasks.txt")
+
 
 
 def load_tasks():
@@ -20,7 +22,6 @@ def save_tasks(tasks):
 @app.route("/", methods=["GET", "POST"])
 def todo_app():
     tasks = load_tasks() 
-
     if request.method == "POST":
         task = request.form.get("task_content")
         if task:
@@ -38,6 +39,20 @@ def delete_task(task_id):
         tasks.pop(task_id)  
         save_tasks(tasks)  
     return redirect(url_for("todo_app")) 
+
+@app.route("/edit/<int:task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    tasks=load_tasks()
+    if task_id < 0 or task_id >= len(tasks):
+        return redirect(url_for("todo_app"))
+    if request.method == "POST":
+        new_task = request.form.get("updated_content")
+        if new_task:
+            tasks[task_id] = new_task
+            save_tasks(tasks)
+        return redirect(url_for("todo_app"))
+
+    return render_template("edit.html", current_task=tasks[task_id], task_id=task_id)
 
 
 if __name__ == "__main__":
